3f01456cf4c24a9cac8a7d176f7a5fc01e351cb7
gemore <gerardo.eleazar.morelli@monsanto.com>
2017-04-20 16:32:17 -0300

    Scroll to Top and Bottom

--- src/pages/main/buttons/buttons.ts
+++ src/pages/main/buttons/buttons.ts
@@ -1,46 +1,59 @@
-import {Component} from "@angular/core";
+import {Component, EventEmitter, Output} from "@angular/core";
 import {EffectsService} from "../../../providers/effects-service";
 
 @Component({
   selector: 'buttons',
   templateUrl: 'buttons.html'
 })
-
 export class Buttons {
+
+  @Output()
+  scrollHandler: EventEmitter<string> = new EventEmitter();
 
   constructor(public effectsService: EffectsService) {
   }
 
   drawCriticalSlashingEffect() {
     this.effectsService.drawCriticalSlashingEffect();
+    this.emitScrollToBottom();
   }
 
   drawCriticalBludgeoningEffect() {
     this.effectsService.drawCriticalBludgeoningEffect();
+    this.emitScrollToBottom();
   }
 
   drawCriticalPiercingEffect() {
     this.effectsService.drawCriticalPiercingEffect();
+    this.emitScrollToBottom();
   }
 
   drawCriticalMagicalEffect() {
     this.effectsService.drawCriticalMagicalEffect();
+    this.emitScrollToBottom();
   }
 
   drawFumbleMeleeEffect() {
     this.effectsService.drawFumbleMeleeEffect();
+    this.emitScrollToBottom();
   }
 
   drawFumbleRangedEffect() {
     this.effectsService.drawFumbleRangedEffect();
+    this.emitScrollToBottom();
   }
 
   drawFumbleNaturalEffect() {
     this.effectsService.drawFumbleNaturalEffect();
+    this.emitScrollToBottom();
   }
 
   drawFumbleMagicalEffect() {
     this.effectsService.drawFumbleMagicalEffect();
+    this.emitScrollToBottom();
   }
 
+  private emitScrollToBottom() {
+    this.scrollHandler.emit('SCROLL_TO_BOTTOM');
+  }
 }
--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,5 +1,5 @@
-import {Component} from "@angular/core";
-import {NavController} from "ionic-angular";
+import {Component, ViewChild} from "@angular/core";
+import {Content, NavController} from "ionic-angular";
 import {EffectsService} from "../../providers/effects-service";
 import {Buttons} from "./buttons/buttons";
 import {Effects} from "./effects/effects";
@@ -12,11 +12,21 @@ import {Effects} from "./effects/effects";
 })
 export class MainPage {
 
+  @ViewChild(Content) content: Content;
+
   constructor(public navCtrl: NavController, public effectsService: EffectsService) {
   }
 
   clearEffects() {
     this.effectsService.clearEffects();
+    this.content.scrollToTop();
   }
 
+  scroll(to: string) {
+    if (to === 'SCROLL_TO_TOP') {
+      this.content.scrollToTop();
+    } else if (to === 'SCROLL_TO_BOTTOM') {
+      this.content.scrollToBottom();
+    }
+  }
 }
