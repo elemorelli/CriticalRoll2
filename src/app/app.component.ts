c3c5f34ef2216de5d573073e421ca3b9d27d4e1a
Eleazar Morelli <gemorelli@gmail.com>
2017-05-11 22:07:52 -0300

    Slider for help menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import {Component, ViewChild} from "@angular/core";
 import {MenuController, Nav, Platform} from "ionic-angular";
 import {StatusBar} from "@ionic-native/status-bar";
 import {SplashScreen} from "@ionic-native/splash-screen";
+import {Storage} from "@ionic/storage";
 
 import {MainPage} from "../pages/main/main";
 import {HelpPage} from "../pages/help/help";
 import {GlossaryPage} from "../pages/glossary/glossary";
 import {SettingsPage} from "../pages/settings/settings";
 import {LicensePage} from "../pages/license/license";
-import {SettingsService} from "../providers/settings-service";
 
 @Component({
   templateUrl: 'app.html'
@@ -16,14 +16,14 @@ import {SettingsService} from "../providers/settings-service";
 export class MyApp {
   @ViewChild(Nav)
   nav: Nav;
-  rootPage = MainPage;
+  rootPage: any = MainPage;
   pages: Array<{ title: string, component: any, icon: string }>;
 
   constructor(public platform: Platform,
               public menu: MenuController,
               public statusBar: StatusBar,
               public splashScreen: SplashScreen,
-              public settingsService: SettingsService) {
+              public storage: Storage) {
     this.initializeApp();
 
     this.pages = [
@@ -37,7 +37,15 @@ export class MyApp {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      this.storage.get('introShown').then((result) => {
+        if (!result) {
+          this.rootPage = MainPage;
+        } else {
+          this.rootPage = HelpPage;
+          this.storage.set('introShown', true);
+        }
+        this.splashScreen.hide();
+      });
     });
   }
 
--- src/pages/help/help.ts
+++ src/pages/help/help.ts
@@ -1,10 +1,16 @@
 import {Component} from "@angular/core";
+import {SettingsService} from "../../providers/settings-service";
+import {NavController} from "ionic-angular";
 
 @Component({
   selector: 'page-help',
   templateUrl: 'help.html'
 })
 export class HelpPage {
-  constructor() {
+
+  public system: string;
+
+  constructor(public navCtrl: NavController, private settings: SettingsService) {
+    this.system = settings.getSystem();
   }
 }
